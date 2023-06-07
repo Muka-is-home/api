2c8453c6cd1677a8695a8d7350913db407cf628f
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-06-07 11:35:50 -0500

    updated user model

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 from .user_type import UserType
 
 class User(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=55)
   website = models.URLField()
   bio = models.TextField()
