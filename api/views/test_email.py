bf3f8b5376c7a5f0eb327ffd6b2dac9684f91f1a
Trinity Christiana <trinitycterry@gmail.com>
2023-07-06 17:20:07 -0500

    Merge branch 'main' into 15-get-backend-repo-ready-for-development

---
+++ api/views/test_email.py
@@ -0,0 +1,24 @@
+"""style view"""
+
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
+from rest_framework.response import Response
+from django.core.mail import EmailMessage
+
+class EmailView(ViewSet):
+    """Email views"""
+
+    def list(self, request):
+        """test sending email for Muka"""
+
+        email = EmailMessage(
+          'Test for Muka',  # Email subject
+          'This is a test',  # Email message
+          'help.muka@gmail.com',  # Sender email
+          ['help.muka@gmail.com'],
+          # List of recipient emails, add yours if you'd like otherwise it's just sending to itself
+        )
+
+        email.send()
+
+        return Response({'message': 'Email Sent!'}, status=status.HTTP_204_NO_CONTENT)
