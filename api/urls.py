cb39a3e160ca9a9bd1d585d98cdb26999adbc452
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-07-08 20:33:36 -0500

    moved api urls to api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 # https://www.django-rest-framework.org/api-guide/routers/
 from rest_framework import routers
+from django.urls import path, include
 from .views import ShopItemView, LogoView, EmailView, ContentView
 
 
@@ -7,3 +8,8 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'emails', EmailView, 'email')
 router.register(r'shop', ShopItemView, 'shop_item')
 router.register(r'logos', LogoView, 'logo')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/content', ContentView.as_view())
+]
