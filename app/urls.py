fa68828f4bfe3e9488a952e10f874c5a0faa5487
Trinity Christiana <trinitycterry@gmail.com>
2023-07-08 20:14:41 -0500

    Added new Admin app

--- app/urls.py
+++ app/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from api.urls import router
+from adminapp.urls import urlpatterns as admin_urlpatterns
 
 urlpatterns = [
     path("api/", include(router.urls)),
-    path("admin/", admin.site.urls),
 ]
+
+urlpatterns += admin_urlpatterns
