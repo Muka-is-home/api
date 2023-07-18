e3b6073b5a0d70a369caf31c2e53399457d1012b
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-07-17 19:22:06 -0500

    readded search url and changed user serializer to limit response

--- api/serializers/serializers.py
+++ api/serializers/serializers.py
@@ -1,28 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from api.models import User, Content, Specialization
 
-class UserSerializer(ModelSerializer):
-    """serializer for blog content"""
+class UserSerializer(serializers.ModelSerializer):
+    """serializer for user information"""
 
     class Meta:
-        """fields"""
         model = User
-        fields = ('name', 'website', 'bio', 'user', 'user_county')
+        fields = ('name', 'website', 'bio', 'email')
         depth = 2
 
-class ContentSerializer(ModelSerializer):
+    email = serializers.EmailField(source='user.email')
+
+class ContentSerializer(serializers.ModelSerializer):
     """serializer for blog content"""
 
     class Meta:
-        """fields"""
         model = Content
         fields = ('id', 'title', 'body', 'author', 'content_type', 'date')
         depth = 1
 
-class SpecializationSerializer(ModelSerializer):
+class SpecializationSerializer(serializers.ModelSerializer):
     """serializer for vendor specializations"""
 
     class Meta:
-        """fields"""
         model = Specialization
         fields = ('id', 'tag_name', 'description', 'on_homepage')
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 # https://www.django-rest-framework.org/api-guide/routers/
 from rest_framework import routers
 from django.urls import path, include
-from .views import ShopItemView, LogoView, EmailView, ContentView, SpecializationView
+from .views import ShopItemView, LogoView, EmailView, ContentView, SpecializationView, SearchView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'emails', EmailView, 'email')
 router.register(r'shop', ShopItemView, 'shop_item')
 router.register(r'logos', LogoView, 'logo')
+router.register(r'search', SearchView, 'search')
 
 urlpatterns = [
     path('api/', include(router.urls)),
