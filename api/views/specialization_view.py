8b47b3f815bbabd4a4a9e4c785772110e8f2787f
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-08-10 10:18:16 -0500

    removed user filtering from specialization view

--- api/views/specialization_view.py
+++ api/views/specialization_view.py
@@ -1,6 +1,6 @@
 from rest_framework import filters, generics
-from api.models import Specialization, User
-from api.serializers import SpecializationSerializer, UserSerializer
+from api.models import Specialization
+from api.serializers import SpecializationSerializer
 
 class CustomSearchFilter(filters.SearchFilter):
     def get_search_fields(self, view, request):
@@ -11,16 +11,8 @@ class SpecializationView(generics.ListAPIView):
     filter_backends = [CustomSearchFilter]
     serializer_class = SpecializationSerializer
 
-    def get_serializer_class(self):
-        if self.request.query_params.get('service'):
-            return UserSerializer
-        return super().get_serializer_class()
-
     def get_queryset(self):
         home_page = self.request.query_params.get('homePage')
-        specialization = self.request.query_params.get('service')
         if home_page:
             return Specialization.objects.filter(on_homepage=True)
-        if specialization:
-            return User.objects.filter(user_specialization__specialization__tag_name=specialization)
         return super().get_queryset()
