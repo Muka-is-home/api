24ba4d578dd9ee117e7d8f9b16e08143f8b66629
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-08-10 11:07:21 -0500

    added endpoints for Realtors

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -5,3 +5,4 @@ from .home_page_search import SearchView
 from .content_view import ContentView
 from .specialization_view import SpecializationView
 from .vendor_view import VendorView
+from .agent_view import AgentView
