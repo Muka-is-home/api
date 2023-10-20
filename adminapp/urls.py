00c13a1010b447b13c76fcbe855288f1f8486955
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-10-20 13:24:14 -0500

    added user login view

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,8 +1,9 @@
 # https://www.django-rest-framework.org/api-guide/routers/
 from django.urls import path
+from adminapp.views import UserLoginView
 
 app_name = "adminapp"
 
 urlpatterns = [
-
+  path(f"{app_name}/login", UserLoginView.as_view(), name="user_login")
 ]
--- adminapp/views/login.py
+++ adminapp/views/login.py
@@ -1,16 +1,21 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-
+from adminapp.forms import CustomAuthenticationForm
+from api.models import User
 
 class UserLoginView(LoginView):
     redirect_authenticated_user = True
-    template_name = 'adminportal/login.html'
-    authentication_form = AuthenticationForm
+    template_name = 'adminapp/login.html'
+    authentication_form = CustomAuthenticationForm
 
     def get_success_url(self):
-        return reverse_lazy('user_list')
+        try:
+          user = User.objects.get(user=self.request.user)
+          if user.active:
+            return reverse_lazy('user_detail', args=[user.id])
+        except User.DoesNotExist:
+          return reverse_lazy('user_list')
 
     def form_invalid(self, form):
         messages.error(self.request,'Invalid username or password!')
