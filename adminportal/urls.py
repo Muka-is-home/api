9e0ee48e3241b38dbbcc139cdd91372fb9647a50
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-10-20 13:48:30 -0500

    added app_name to urls

--- adminportal/urls.py
+++ adminportal/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from adminportal.views import MyLoginView, user_list, update_approval
 
+app_name = 'adminportal'
+
 urlpatterns = [
-  path("login", MyLoginView.as_view(), name="login"),
-  path("user_list", user_list, name="user_list"),
-  path("update_approval", update_approval, name="update_approval")
+  path(f"{app_name}/login", MyLoginView.as_view(), name="login"),
+  path(f"{app_name}/user_list", user_list, name="user_list"),
+  path(f"{app_name}/update_approval", update_approval, name="update_approval")
 ]
