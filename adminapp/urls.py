be06e8db1060d09df49cb85e39163624a845f2ba
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-10-20 13:30:04 -0500

    added user_detail url

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,5 +5,6 @@ from adminapp.views import UserLoginView
 app_name = "adminapp"
 
 urlpatterns = [
-  path(f"{app_name}/login", UserLoginView.as_view(), name="user_login")
+  path(f"{app_name}/login", UserLoginView.as_view(), name="user_login"),
+  path("user_detail/<int:pk>", user_detail, name="user_detail"),
 ]
