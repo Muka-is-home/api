62025d5c16c657c95594e8f010d9edcc86be852d
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-10-25 13:09:25 -0500

    added urls for profile forms and signup

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,10 +1,13 @@
 # https://www.django-rest-framework.org/api-guide/routers/
 from django.urls import path
-from adminapp.views import UserLoginView, user_detail
+from adminapp.views import UserLoginView, user_detail, signup
 
 app_name = "adminapp"
 
 urlpatterns = [
   path(f"{app_name}/login", UserLoginView.as_view(), name="user_login"),
   path(f"{app_name}/user_detail/<int:pk>", user_detail, name="user_detail"),
+  path(f"{app_name}/signup", signup, name="signup"),
+  path(f"{app_name}/realtor_profile", name="realtor_profile"),
+  path(f"{app_name}/vendor_profile", name="vendor_profile")
 ]
--- adminapp/views/__init__.py
+++ adminapp/views/__init__.py
@@ -1,2 +1,3 @@
 from .login import UserLoginView
 from .user_detail import user_detail
+from .signup import signup
