3dca6670ee13aa80b93c5f2321362c68be15fa85
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-11-21 21:09:02 -0600

    added pagination to user list

--- adminportal/views/user_list.py
+++ adminportal/views/user_list.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from api.models import User
 from adminportal.serializers.serializers import UserListSerializer
 
@@ -8,8 +9,19 @@ def user_is_superuser(user):
 
 @user_passes_test(user_is_superuser, login_url="access_denied")
 def user_list(request):
-    users = User.objects.exclude(user__is_superuser=True)
-    user_data = UserListSerializer(users, many=True).data
+    # order by descending id so newest additions will show first
+    user_list = User.objects.exclude(user__is_superuser=True).order_by('-id')
+    
+    paginator = Paginator(user_list, 10)
+    page = request.GET.get('page')
+
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+    
     return render(request, "adminportal/user_list.html", {
-        "users": user_data
+        "users": users
     })
