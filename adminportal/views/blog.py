4a20a1dc6c17c6ed873575f301b41ace47fbba8f
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-11-21 21:20:37 -0600

    added pagination to blog list

--- adminportal/views/blog.py
+++ adminportal/views/blog.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from api.models import Content, ContentTag, ContentType, User, Tag
 from api.serializers import ContentSerializer, ContentTypeSerializer, TagSerializer
 from adminportal.views import user_is_superuser
@@ -98,8 +99,17 @@ def edit_blog(request, pk):
 
 @user_passes_test(user_is_superuser, login_url="access_denied")
 def blog_list(request):
-    blog_posts = Content.objects.all()
-    blog_data = ContentSerializer(blog_posts, many=True).data
+    blog_list = Content.objects.all().order_by('-date')
+
+    paginator = Paginator(blog_list, 10)
+    page = request.GET.get('page')
+
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
     return render(request, "adminportal/blog_list.html", {
-        "blogs": blog_data
+        "blogs": blogs
     })
