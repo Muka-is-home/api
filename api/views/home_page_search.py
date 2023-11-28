21ca67393de00ec9253e59ee1e56c29445eaa2bc
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-11-28 13:09:23 -0600

    home page search only returns active users

--- api/views/home_page_search.py
+++ api/views/home_page_search.py
@@ -20,7 +20,7 @@ class SearchView(ViewSet):
             counties = UserCounty.objects.filter(user__user_type__name=user_type, county__state__name=state)
             response = set([county.county.name for county in counties])
         else:
-            user_list = users.filter(user_county__county__name=county, user_county__county__state__name=state)
+            user_list = users.filter(user_county__county__name=county, user_county__county__state__name=state, active=True)
             serializer = UserSerializer(user_list, many=True)
             response = serializer.data
 
