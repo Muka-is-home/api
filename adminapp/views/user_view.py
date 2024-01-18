743f24e059fff9c25dab927885088c4cff5b72a0
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-01-17 21:00:02 -0600

    added password reset functionality

--- adminapp/views/user_view.py
+++ adminapp/views/user_view.py
@@ -28,9 +28,13 @@ def edit_profile(request, pk, type):
             if image is not None:
                 user.image = image
             
+            email = request.POST.get("email")
+            user.user.email = email
+            user.user.save()
+            
             user.name=name
             user.website=request.POST.get("website")
-            user.email=request.POST.get("email")
+            user.email=email
             user.bio=request.POST.get("bio")
             user.company=request.POST.get("company")
             user.company_address=request.POST.get("company_address")
@@ -91,14 +95,19 @@ def user_detail(request, pk):
     
 @login_required
 def create_profile(request, type):
+    
     if request.method == "POST":
-        if User.objects.filter(email=request.POST.get("email")):
+        email = request.POST.get("email")
+        
+        if User.objects.filter(email=email):
             return render(request, "adminapp/email_taken.html", {
                 "type": type
             })
         
         else:
             auth_user = AuthUser.objects.get(username=request.user)
+            auth_user.email = email
+            auth_user.save()
             user_type = UserType.objects.get(name=type)
             
             name = request.POST.get("name")
@@ -106,7 +115,7 @@ def create_profile(request, type):
             user_profile = User(
                 user=auth_user,
                 name=name,
-                email=request.POST.get("email"),
+                email=email,
                 website=request.POST.get("website"),
                 bio=request.POST.get("bio"),
                 company=request.POST.get("company"),
