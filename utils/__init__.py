8b306a7b2c33df2ff4634a636fcad9237c5cab88
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-01-17 20:00:38 -0600

    modify yser emails and sign up messages

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .get_env import get_env, get_env_names
 from .image_uploader import handle_image_upload
-from .email_templates import THANK_YOU_TEMPLATE, REJECTION_TEMPLATE, APPROVAL_TEMPLATE
+from .email_templates import signup_email_body, approval_email_body, rejection_email_body
